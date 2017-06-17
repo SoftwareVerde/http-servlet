7567d219f02fdca5d244de02a2a62e09acffbaf4
Josh Green <josh@softwareverde.com>
2017-06-17 12:17:05 -0400

    Updated scripts.

--- scripts/make-jar.sh
+++ scripts/make-jar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./gradlew makeJar
+rm -rf out/bin 2>/dev/null
+mkdir -p out/bin
 
-echo -e "\nJar Location: "
-ls -tr build/libs/*.jar | tail -1
+./gradlew makeJar && cp build/libs/*.jar out/bin/. && chmod 770 out/bin/*.jar
+
