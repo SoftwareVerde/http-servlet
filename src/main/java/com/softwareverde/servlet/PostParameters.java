7890cb9e1ab860d8328aa1789436d814602ecfe4
Josh Green <josh@softwareverde.com>
2017-06-17 12:13:43 -0400

    Extracted TomcatShim into separate project.

--- src/main/java/com/softwareverde/servlet/PostParameters.java
+++
@@ -1,5 +0,0 @@
-package com.softwareverde.servlet;
-
-import com.softwareverde.servlet.querystring.QueryString;
-
-public class PostParameters extends QueryString { }
--- src/main/java/com/softwareverde/servlet/Servlet.java
+++
@@ -1,8 +0,0 @@
-package com.softwareverde.servlet;
-
-import com.softwareverde.servlet.request.Request;
-import com.softwareverde.servlet.response.Response;
-
-public interface Servlet {
-    Response onRequest(Request request);
-}
