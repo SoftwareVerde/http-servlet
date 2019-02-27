29c252ac810dd75af0a3edc89c22b13956775059
Josh Green <josh@softwareverde.com>
2019-02-27 00:14:29 -0500

    FIX: TLS redirection now provides correct url with nonstandard port.

--- src/main/java/com/softwareverde/httpserver/endpoint/EncryptionRedirectEndpoint.java
+++ src/main/java/com/softwareverde/httpserver/endpoint/EncryptionRedirectEndpoint.java
@@ -1,9 +1,12 @@
 package com.softwareverde.httpserver.endpoint;
 
 import com.softwareverde.servlet.Servlet;
+import com.softwareverde.servlet.request.Headers;
 import com.softwareverde.servlet.request.Request;
 import com.softwareverde.servlet.response.Response;
 import com.softwareverde.util.Util;
+
+import java.util.List;
 
 public class EncryptionRedirectEndpoint implements Servlet {
     public static final Integer standardHttpsPort = 443;
@@ -12,16 +15,37 @@ public class EncryptionRedirectEndpoint implements Servlet {
     public void setTlsPort(final Integer port) { _tlsPort = port; }
     public Integer getTlsPort() { return _tlsPort; }
 
+    protected String _getServerHostnameWithPort(final Request request) {
+        final Headers headers = request.getHeaders();
+        if (headers.containsHeader("host")) {
+            final List<String> headerValues = headers.getHeader("host");
+            final String hostHeaderValue = Util.coalesce(headerValues.get(0));
+            if (! hostHeaderValue.isEmpty()) {
+                return hostHeaderValue;
+            }
+        }
+
+        return request.resolveHostname();
+    }
+
+    protected String _getServerHostname(final Request request) {
+        final String hostnameAndPort = _getServerHostnameWithPort(request);
+        if (! hostnameAndPort.contains(":")) { return hostnameAndPort; }
+
+        final int endIndex = hostnameAndPort.indexOf(':');
+        return hostnameAndPort.substring(0, endIndex);
+    }
+
     @Override
     public Response onRequest(final Request request) {
         final Response response = new Response();
 
         final String newUrl;
         {
-            final String host = request.getHostname();
+            final String hostname = _getServerHostname(request);
             final String url = request.getFilePath() + Util.coalesce(request.getQueryString());
             final Boolean requiresTlsPort = (! _tlsPort.equals(standardHttpsPort));
-            newUrl = host + (requiresTlsPort ? ":"+ _tlsPort : "") + url;
+            newUrl = hostname + (requiresTlsPort ? ":"+ _tlsPort : "") + url;
         }
 
         response.addHeader("Location", "https://"+ newUrl);
